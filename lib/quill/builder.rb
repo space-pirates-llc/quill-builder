c79ac7daf6baeeaa04b4e410760f9e3792f4f247
Code Ahss <aycabta@gmail.com>
2017-10-09 03:43:28 +0900

    Initial commit

---
+++ lib/quill/builder.rb
@@ -0,0 +1,7 @@
+require "quill/builder/version"
+
+module Quill
+  module Builder
+    # Your code goes here...
+  end
+end
---
+++ lib/quill/builder/version.rb
@@ -0,0 +1,5 @@
+module Quill
+  module Builder
+    VERSION = "0.0.1"
+  end
+end
