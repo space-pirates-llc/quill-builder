f115b3cf5a2ebf8b4ad048f2de6396b48e806d0b
Code Ahss <aycabta@gmail.com>
2017-10-25 23:07:57 +0900

    Change architecture of convert for block element

--- lib/quill/builder/html.rb
+++ lib/quill/builder/html.rb
@@ -8,18 +8,27 @@ module Quill::Builder
     end
 
     def convert
-      json = JSON.parse(@text)
-      tags = json['ops'].map do |item|
-        item_to_tag(item)
-      end
-      tags.join
+      tags = convert_intermediate
+      tags.map { |item|
+        text = item[:text].gsub("\n", '<br />')
+        item[:attrs].inject(text) do |memo, tag_pair|
+          "#{tag_pair.first}#{memo}#{tag_pair.last}"
+        end
+      }.join
     end
 
-    def item_to_tag(item)
-      text = item['insert'].gsub("\n", '<br />')
+    def convert_intermediate
+      json = JSON.parse(@text)
+      tags = json['ops'].inject([]) do |memo, item|
+        memo << convert_item(memo, item['insert'], item['attributes'])
+      end
+      tags
+    end
+
+    def convert_item(memo, text, attributes)
       attrs = []
-      if item['attributes']
-        item['attributes'].each_pair do |key, value|
+      if attributes
+        attributes.each_pair do |key, value|
           case key
           when 'bold'
             attrs << ['<b>', '</b>']
@@ -33,16 +42,30 @@ module Quill::Builder
             attrs << [%Q|<span style="background-color: #{value}">|, '</span>']
           when 'color'
             attrs << [%Q|<span style="color: #{value}">|, '</span>']
+          when 'blockquote'
+            text = normalize_block(memo, text)
+            attrs << ['<blockquote>', '</blockquote>']
+          when 'code-block'
+            text = normalize_block(memo, text)
+            attrs << ['<pre>', '</pre>']
           end
         end
       end
-      if attrs.empty?
-        text
-      else
-        attrs.inject(text) do |memo, tag_pair|
-          "#{tag_pair.first}#{memo}#{tag_pair.last}"
-        end
+      {
+        text: text,
+        attrs: attrs
+      }
+    end
+
+    def normalize_block(memo, text)
+      last = memo.last
+      if last && text[0] == "\n" && last[:text][-1] != "\n"
+        splited = last[:text].split(/(?<=\n)/)
+        last[:text] = splited[0..-2].join("\n")
+        text = splited.last + text[1..-1]
+        memo.pop if last[:text].empty?
       end
+      text
     end
   end
 end
