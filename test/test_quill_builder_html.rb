c9b70f2710dbabe8cec4fcf643187e55506912c6
Code Ahss <aycabta@gmail.com>
2017-10-30 05:08:47 +0900

    Fix some tests for complex newline handling

--- test/test_quill_builder_html.rb
+++ test/test_quill_builder_html.rb
@@ -132,4 +132,105 @@ class Quill::Builder::HTML::Test < Test::Unit::TestCase
     ]
     assert_equal(expect, output)
   end
+
+  def test_convert_block_with_prev_text
+    input = {
+      ops: [
+        { insert: "a\nb" },
+        {
+          attributes: { blockquote: true },
+          insert: "\n"
+        }
+      ]
+    }
+    output = Quill::Builder::HTML.new(input.to_json).convert_to_lines
+    expect = [
+      {
+        block: :p,
+        inlines: [ { attrs: [], text: "a\n" } ]
+      },
+      {
+        block: [ :p, :blockquote ],
+        inlines: [ { attrs: [], text: 'b' } ]
+      }
+    ]
+    assert_equal(expect, output)
+  end
+
+  def test_convert_block_with_next_inline
+    input = {
+      ops: [
+        { insert: 'a' },
+        {
+          attributes: { blockquote: true },
+          insert: "\n"
+        },
+        {
+          attributes: { bold: true },
+          insert: 'b'
+        },
+        { insert: "\n"}
+      ]
+    }
+    output = Quill::Builder::HTML.new(input.to_json).convert_to_lines
+    expect = [
+      {
+        block: [ :p, :blockquote ],
+        inlines: [ { attrs: [], text: 'a' } ]
+      },
+      {
+        block: :p,
+        inlines: [ { attrs: [['<b>', '</b>']], text: 'b' } ]
+      }
+    ]
+    assert_equal(expect, output)
+  end
+
+  def test_convert_complex
+    input = {
+      ops: [
+        { insert: "a"},
+        { attributes: { blockquote: true }, insert: "\n" },
+        { attributes: { bold: true }, insert: 'b' },
+        { insert: "\n\n\n" },
+        { attributes: { bold: true }, insert: 'a' },
+        { insert: "\n" }
+      ]
+    }
+    output = Quill::Builder::HTML.new(input.to_json).convert_to_lines
+    expect = [
+      {
+        block: [ :p, :blockquote ],
+        inlines: [
+          { text: 'a', attrs: [] }
+        ]
+      },
+      {
+        block: :p,
+        inlines: [
+          { text: 'b', attrs: [['<b>', '</b>']] },
+          { text: "\n", attrs: [] }
+        ]
+      },
+      {
+        block: :p,
+        inlines: [
+          { text: "\n", attrs: [] }
+        ]
+      },
+      {
+        block: :p,
+        inlines: [
+          { text: "\n", attrs: [] }
+        ]
+      },
+      {
+        block: :p,
+        inlines: [
+          { text: "a", attrs: [["<b>", "</b>"]] }
+        ]
+      }
+    ]
+    assert_equal(expect, output)
+  end
 end
